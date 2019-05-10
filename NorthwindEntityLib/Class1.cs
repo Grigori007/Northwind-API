af03042614245f6a8e88de6d0ac529ddea337a1a
Grzegorz Kopeć <01113975@pw.edu.pl>
2019-05-10 13:08:19 +0200

    Initial commit;

--- NorthwindEntityLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NorthwindEntityLib
-{
-    public class Class1
-    {
-    }
-}
---
+++ NorthwindEntityLib/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindEntityLib
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int? SupplierID { get; set; }
+        public Supplier Supplier { get; set; }
+        public int? CategoryID { get; set; }
+        public Category Category { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; } = 0;
+        public short? UnitsInStock { get; set; } = 0;
+        public short? UnitsInOrder { get; set; } = 0;
+        public short? RecorderLevel { get; set; } = 0;
+        public bool IsDiscontinued { get; set; } = false;
+    }
+}
