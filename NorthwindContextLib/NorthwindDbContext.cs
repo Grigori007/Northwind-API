5d18419544eca31b6245810a8931f5a62bbafc2e
Grzegorz Kopeć <01113975@pw.edu.pl>
2019-05-10 15:28:46 +0200

    NorthwindDbContext added;

---
+++ NorthwindContextLib/NorthwindDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using NorthwindEntityLib;
+
+namespace NorthwindContextLib
+{
+    public class NorthwindDbContext : DbContext
+    {
+        public NorthwindDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>()
+                .Property(c => c.CategoryName)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            // definig one-to-many relationship
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(p => p.Category);
+
+
+        }
+    }
+}
+    
