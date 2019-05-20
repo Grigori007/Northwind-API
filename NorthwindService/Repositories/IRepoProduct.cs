07a31cfeeead5e3026bd8429a1675c764998fc03
Grigori007 <grzegorz.kopec05@gmail.com>
2019-05-20 23:43:58 +0200

    IRepoProduct added;

---
+++ NorthwindService/Repositories/IRepoProduct.cs
@@ -0,0 +1,16 @@
+﻿using NorthwindContextLib;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace NorthwindService.Repositories
+{
+    public interface IRepoProduct
+    {
+        Task<Product> CreateAsync(Product product);
+        Task<IEnumerable<Product>> ReadAllAsync();
+        Task<Product> ReadAsync(string id);
+        Task<Product> UpdateAsync(Product product);
+        Task<bool> DeleteAsync(string id);
+    }
+}
